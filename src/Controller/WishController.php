743394e2bfa25715d1412d1582bc60f28e37d259
claudiekante <claudiekante@gmail.com>
2022-10-05 16:03:48 +0200

    page de bases pour les series

---
+++ src/Controller/WishController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WishController extends AbstractController
+{
+    /**
+     * @Route("/wish", name="wish_list")
+     */
+    public function list(): Response
+    {
+        return $this->render('wish/list.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/wish/detail/{id}", name="wish_detail")
+     */
+    public function detail(int $id): Response
+    {
+        return $this->render('wish/detail.html.twig', [
+
+        ]);
+    }
+
+}
